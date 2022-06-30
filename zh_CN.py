23958f87f1723c7c344536c916f580fcde233a6d
TuanZiGit <318504340@qq.com>
2022-06-30 18:32:54 +0800

    Sync 20220630 - Update

--- zh_CN.py
+++ zh_CN.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78b29b5a9c0796502c327a62a461e007c53dc5214cd49afe21992461e60c7020
-size 1567
+oid sha256:fb3bb23447cc2167e543d9821175a51521fcae68da1cb69784858d8fdd1bb573
+size 2073
